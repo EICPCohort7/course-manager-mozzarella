5b2fa011b20e37d8aa17247856c346db8ca1634e
Tirth Thakkar <tirth_thakkar@tjxcanada.ca>
2022-10-17 10:50:28 -0400

    implemented dynamic fetch

--- public/students.js
+++ public/students.js
@@ -1,4 +1,4 @@
-import { students } from './data/all-data.js';
+import data, { students } from './data/all-data.js';
 
 let leftSide = document.getElementById('left-side');
 let leftSideList = document.createElement('ul');
@@ -8,6 +8,7 @@ leftSideList.classList.add('list-unstyled');
 
 // Direct style assignment
 leftSideList.style.cursor = 'pointer';
+fetch('http://localhost:8000/students').then((response)=>response.json()).then((data)=>{
 
 for (let student of students) {
   // console.log(student.firstName + ' ' + student.lastName);
@@ -17,21 +18,40 @@ for (let student of students) {
     `<li id="${student.id}">${student.firstName} ${student.lastName}</li>`
   );
 }
-
-// Event delegation!
-leftSideList.addEventListener('click', (event) => {
-  let studentId = event.target.id;
-  let student = students.find((s) => s.id === Number(studentId));
-  // let student = students.find((s) => s.id == studentId);
-  // console.log('You clicked on:', student);
-
-  let rightSide = document.getElementById('right-side');
-  let studentDetails = document.createElement('div');
-  studentDetails.insertAdjacentHTML(
-    'beforeend',
-    `<p>${student.firstName} ${student.lastName} lives in ${student.city}, ${student.province}`
-  );
-  rightSide.replaceChildren(studentDetails);
 });
+
+leftSideList.addEventListener("click", async (event) => {
+
+  let studentId = event.target.id;
+
+  await fetch('http://localhost:8000/students/' + studentId)
+
+  .then((response) => response.json())
+
+  .then((student) => {
+
+    console.log('You clicked on:', student);
+
+   
+
+    let rightSide = document.getElementById('right-side');
+
+    let studentDetails = document.createElement('div');
+
+    studentDetails.insertAdjacentHTML(
+
+      'beforeend',
+
+      `<p>${student.firstName} ${student.lastName} lives in ${student.city}, ${student.province}`
+
+    );
+
+    rightSide.replaceChildren(studentDetails);
+
+  });
+
+});
+
+
 
 leftSide.append(leftSideList);
