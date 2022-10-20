7d075213716c201920420c70c207f6925079d142
John Barber-Ormerod <jack.barber.ormerod@gmail.com>
2022-10-20 11:11:44 -0400

    Improved drop downs

--- public/drop-downs.js
+++ public/drop-downs.js
@@ -1,21 +1,45 @@
-
-document.getElementById('state_selector').style.visibility = 'hidden';
-document.getElementById('province_selector').style.visibility = 'visible';
+function removeOptions(selectElement) {
+    let i, L = selectElement.options.length - 1;
+    for(i = L; i >= 0; i--) {
+       selectElement.remove(i);
+    }
+ }
 let selector = document.getElementById('country');
 selector.addEventListener("change", ()=>{
     let country = document.getElementById("country").value
-    if (country === "Canada")
-        {
-            document.getElementById('state_selector').style.visibility = 'hidden';
-            document.getElementById('province_selector').style.visibility = 'visible';
-        }
-    if(country === "US"){
-        document.getElementById('state_selector').style.visibility = 'visible';
-        document.getElementById('province_selector').style.visibility = 'hidden'
-    }
     if(country === "UK"){
-        document.getElementById('state_selector').style.visibility = 'hidden';
-        document.getElementById('province_selector').style.visibility = 'hidden'
+        removeOptions(document.getElementById('province/state'));
+        document.getElementById('provincestate_selector').style.visibility = 'hidden'
+    }
+    if(country== "US"){
+        removeOptions(document.getElementById('province/state'));
+        addOptions("US")
+        document.getElementById('provincestate_selector').style.visibility = 'visible'
+    }
+    if(country== "Canada"){
+        removeOptions(document.getElementById('province/state'));
+        addOptions("Canada")
+        document.getElementById('provincestate_selector').style.visibility = 'visible'
     }
 
 });
+function addOptions(country){
+let select = document.getElementById("province/state");
+let options = []
+if (country === "Canada"){
+    options = ["Ontario", "Quebec", "Manitoba", "Northwest Territories", "Nunavut", "Yukon",
+        "British Columbia","Albeta","Saskatchewan","New Brunswick","Newfounland and Labrador","Prince Edward Island","Nova Scotia"];
+}
+else if (country == "US"){
+    options = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", 
+    "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+}
+
+for(let i = 0; i < options.length; i++) {
+    let opt = options[i];
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+}
+}
