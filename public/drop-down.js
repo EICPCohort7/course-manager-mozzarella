95ba83b0a45ed752177758be6450c7030f598ff8
Jaana Dominique Tabalon <jaanatabalon@gmail.com>
2022-10-20 11:10:14 -0400

    Change province/state dropdown based on country

--- public/drop-down.js
+++ public/drop-down.js
@@ -1,22 +1,54 @@
 import { provinces } from './data/ca-provinces.js';
 import { usStates } from './data/states.js';
 
-let stateProvinceDropdown = document.getElementById('province');
+let provinceDropdown = document.getElementById('province');
+let stateDropdown = document.getElementById('state');
 let countryDropdown = document.getElementById('country');
 let countrySelection = document.getElementById('country').value;
 
-countryDropdown.addEventListener('click', () => {
+provinceDropdown.style.visibility = 'visible';
+stateDropdown.style.visibility = 'hidden';
+
+countryDropdown.addEventListener('change', () => {
   if (countrySelection === 'ca') {
     for (let p of provinces) {
-      stateProvinceDropdown.insertAdjacentHTML('beforeend',
+      provinceDropdown.style.visibility = 'visible';
+      stateDropdown.style.visibility = 'hidden';
+      provinceDropdown.insertAdjacentHTML('beforeend',
         `<option value="${p.abbr}">${p.name}</option>`);
     }
   } else if (countrySelection === 'us') {
     for (let s of usStates) {
-      stateProvinceDropdown.insertAdjacentHTML('beforeend',
+      provinceDropdown.style.visibility = 'hidden';
+      stateDropdown.style.visibility = 'visible';
+      stateDropdown.insertAdjacentHTML('beforeend',
         `<option value="${s.abbreviation}">${s.name}</option>`);
     }
   } else {
-    stateProvinceDropdown.style.display = 'none';
+    provinceDropdown.style.visibility = 'hidden';
+    stateDropdown.style.visibility = 'hidden';
   }
 });
+
+/**
+ * document.getElementById('state_selector').style.visibility = 'hidden';
+document.getElementById('province_selector').style.visibility = 'visible';
+let selector = document.getElementById('country');
+selector.addEventListener("change", ()=>{
+    let country = document.getElementById("country").value
+    if (country === "Canada")
+        {
+            document.getElementById('state_selector').style.visibility = 'hidden';
+            document.getElementById('province_selector').style.visibility = 'visible';
+        }
+    if(country === "US"){
+        document.getElementById('state_selector').style.visibility = 'visible';
+        document.getElementById('province_selector').style.visibility = 'hidden'
+    }
+    if(country === "UK"){
+        document.getElementById('state_selector').style.visibility = 'hidden';
+        document.getElementById('province_selector').style.visibility = 'hidden'
+    }
+
+});
+ */
