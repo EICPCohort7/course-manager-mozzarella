60b6d99a912d0561420c66c4b7e1ed966a53554b
Tirth Thakkar <tirth_thakkar@tjxcanada.ca>
2022-10-24 11:03:54 -0400

    Working add button

--- public/js/add-student.js
+++ public/js/add-student.js
@@ -2,6 +2,16 @@ import { linkLists } from './link-lists.js';
 
 let countriesSelect = document.getElementById('country');
 let provincesSelect = document.getElementById('province');
+let form = document.getElementById('demo-form');
 linkLists(countriesSelect, provincesSelect);
 
-const submitBtn = document.getElementById("");
+const submitBtn = document.getElementById('demo-form').addEventListener('submit',(event)=>{
+
+    event.preventDefault();
+    let data = new FormData(form);
+
+    for (let entry of data) {
+        console.log(`The ${entry[0]} field has the value ${entry[1]}.`, true);
+      }
+
+});
