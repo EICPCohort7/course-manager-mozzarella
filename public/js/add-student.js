14f74ee901e48ce61b31d60cc083dabfb674db45
Deepti Sachi <deepti_sachi@tjx.com>
2022-10-24 09:55:40 -0500

    Week 3 part 3

--- public/js/add-student.js
+++ public/js/add-student.js
@@ -4,4 +4,19 @@ let countriesSelect = document.getElementById('country');
 let provincesSelect = document.getElementById('province');
 linkLists(countriesSelect, provincesSelect);
 
-const submitBtn = document.getElementById("");
+const form = document.getElementById("form");
+const submitBtn = document.getElementById("form");
+// submitBtn.addEventListener("click",  (event) => {
+//     event.preventDefault();
+// });
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let data = new FormData(form);
+    for (let entry of data) {
+        console.log(`The ${entry[0]} field has the value ${entry[1]}.`, true);
+    }
+
+});
+
